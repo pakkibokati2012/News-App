e5461b124dd955eadfec22d0efa9764639fe1a95
pakkibokati2012 <pakkibokati2012@github.com>
2019-01-04 00:58:09 +0545

    pull down to refresh

--- src/NewsList.js
+++ src/NewsList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList, ActivityIndicator, View, Image, Dimensions, TouchableOpacity, Alert } from 'react-native';
+import { FlatList, ActivityIndicator, View, Image, Dimensions, TouchableOpacity, Alert, RefreshControl } from 'react-native';
 import { Content, Card, CardItem, Text, Body } from 'native-base';
 import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
@@ -7,10 +7,17 @@ import { userOpenedApp } from './actions';
 
 class NewsList extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      refreshing: false
+    };
+  }
 
   componentDidMount() {
     this.props.userOpenedApp();
   }
+
 
   openDetail(detail) {
     if (detail) {
@@ -19,6 +26,20 @@ class NewsList extends React.Component {
       Alert.alert('No detail available for this news!');
     }
   }
+
+  _refreshControl() {
+  return (
+    <RefreshControl
+      refreshing={this.state.refreshing}
+      onRefresh={() => {
+        this.setState({ refreshing: true });
+        this.props.userOpenedApp();
+        this.setState({ refreshing: false })
+      }
+      }
+    />
+  )
+}
 
   render() {
     if (this.props.isLoading) {
@@ -32,11 +53,12 @@ class NewsList extends React.Component {
     return (
         <FlatList
           data={this.props.data.articles}
+          refreshControl={this._refreshControl()}
           renderItem={({ item }) =>
           <TouchableOpacity onPress={() => {
             console.log('item', item.content);
             this.openDetail(item.content);
-          }
+            }
           }
           >
             <Content>
